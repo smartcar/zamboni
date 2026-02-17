b1b95c174c045432667c447d44b6499231a63942
Edward Kelly <edward.kelly@smartcar.com>
2026-02-17 11:04:43 -0800

    feat!: Update to @smartcar/eslint-plugin and eslint 8.x
    BREAKING CHANGE: This moves to eslint 8.x

--- configs/eslintrc-for-all-files.js
+++ configs/eslintrc-for-all-files.js
@@ -2,11 +2,8 @@
 // ESlinting for all files on pre-commit
 // Be careful how many rules we specify as to not slow down commit times!
 
-const config = require('@smartcar/eslint-config');
-
 module.exports = {
-  ...config,
-  extends: [],
+  extends: ['plugin:@smartcar/legacy', 'plugin:@smartcar/mocha'],
   rules: {
     'unicorn/expiring-todo-comments': [
       'error',
